6907138e705f3c2328d12509f5c914a3ef265f33
Sergiy <slevin.uk@gmail.com>
2017-11-12 02:07:05 +0200

    Add City DAL

---
+++ DAL/Abstract/ICityRepository.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Abstract
+{
+    public interface ICityRepository : IDisposable
+    {
+        City Add(City city);
+        IQueryable<City> GetAllCities();
+        City GetCityByName(string name);
+        City GetCityById(int id);
+        int TotalCities();
+        void SaveChange();
+    }
+}
