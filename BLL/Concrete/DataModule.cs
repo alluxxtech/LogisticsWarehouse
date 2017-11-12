ba3099f6b50f4058f859e0559d68f6fbfe0b944f
Sergiy <slevin.uk@gmail.com>
2017-11-12 15:53:17 +0200

    Add AutoFac adn other

---
+++ BLL/Concrete/DataModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using DAL.Abstract;
+using DAL.Concrete;
+using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+    public class DataModule : Module
+    {
+        private string _connStr;
+        public DataModule(string connString)
+        {
+            _connStr = connString;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new EFContext(this._connStr))
+                .As<IEFContext>().InstancePerRequest();
+            builder.RegisterType<CountryRepository>()
+                .As<ICountryRepository>().InstancePerRequest();
+            builder.RegisterType<CityRepository>()
+                .As<ICityRepository>().InstancePerRequest();
+            //builder.RegisterType<UserRepository>()
+            //    .As<IUserRepository>().InstancePerRequest();
+            //builder.RegisterType<AccountIdentityProvider>()
+            //    .As<IAccountProvider>().InstancePerRequest();
+
+
+
+            base.Load(builder);
+        }
+    }
+}
--- LogisticsWarehouse/Startup.cs
+++ LogisticsWarehouse/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(LogisticsWarehouse.Startup))]
-namespace LogisticsWarehouse
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
-    }
-}
+//[assembly: OwinStartupAttribute(typeof(LogisticsWarehouse.Startup))]
+//namespace LogisticsWarehouse
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            ConfigureAuth(app);
+//        }
+//    }
+//}
